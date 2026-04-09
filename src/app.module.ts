c773b8a03af49d5e5d91be1cdd5bf204e30cf398
Lysander-PR <lysanderpr1@gmail.com>
2026-04-08 23:38:47 -0600

    Add TypeORM configuration to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: '',
+      host: '',
+      port: 1111,
+      username: '',
+      password: '',
+      database: '',
+      entities: [],
+      synchronize: false,
+    }),
+  ],
   providers: [],
 })
 export class AppModule {}

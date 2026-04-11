6086ea98b999d03dc1196f2d6f1ccf960eafc5f2
Lysander-PR <lysanderpr1@gmail.com>
2026-04-10 21:03:32 -0600

    Add environment configuration validation using Zod

---
+++ src/config/envs.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+process.loadEnvFile();
+
+const enviromentsSchema = z.object({
+  PORT: z.coerce.number(),
+  DB_PORT: z.coerce.number(),
+  DB_HOST: z.string(),
+  DB_USER: z.string(),
+  DB_PASSWORD: z.string(),
+  DB_NAME: z.string(),
+});
+
+const { error, data } = enviromentsSchema.safeParse(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+export const envs = data;

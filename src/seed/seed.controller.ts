f3f467362fd240547dc091db45fec0f5b58c5486
Lysander-ayudate <Lysander.Paniagua@ayudatelta.com>
2026-04-28 17:28:07 -0600

    seed advance - albums missing

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseFilters } from '@nestjs/common';
 import { SeedService } from './seed.service';
+import { QueryFailedErrorFilter } from '@/common/filters/query-failed.filter';
 
 @Controller('seed')
+@UseFilters(QueryFailedErrorFilter)
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
+
+  @Post()
+  populate() {
+    return this.seedService.populate();
+  }
 }
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -6,9 +6,32 @@ import { Genre } from '@/genres/entities/genre.entity';
 import { Book } from '@/books/entities/book.entity';
 import { Movie } from '@/movies/entities/movie.entity';
 
+import { initialData } from './data/seed.data';
+
 @Injectable()
 export class SeedService {
   constructor(private readonly dataService: DataSource) {}
 
-  populate() {}
+  async populate() {
+    await this.dataService.getRepository(Movie).deleteAll();
+    await this.dataService.getRepository(Book).deleteAll();
+    await this.dataService.getRepository(Genre).deleteAll();
+
+    const { albums, books, genres, movies } = initialData;
+
+    await this.dataService.transaction('SERIALIZABLE', async (manager) => {
+      const moviesEntity = manager.create(Movie, movies);
+      const booksEntity = manager.create(Book, books);
+      const genresEntity = manager.create(
+        Genre,
+        genres.map((genre) => ({ genre })),
+      );
+
+      await manager.save(moviesEntity);
+      await manager.save(booksEntity);
+      const genresDB = await manager.save(genresEntity);
+    });
+
+    return 'SEED EXECUTED';
+  }
 }

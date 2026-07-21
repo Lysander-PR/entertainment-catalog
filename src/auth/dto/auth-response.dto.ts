b23e4d6a22cdb312498932daae743341a7209abf
Lysander-PR <Lysander.Paniagua@ayudatelta.com>
2026-07-21 17:16:41 -0600

    documentation with swagger about the auth module

--- src/auth/dto/auth-response.dto.ts
+++ src/auth/dto/auth-response.dto.ts
@@ -1,6 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { User } from '@/user/entities/user.entity';
 
 export class AuthResponseDto {
+  @ApiProperty({ description: 'Authenticated user', type: User })
   user!: User;
+
+  @ApiProperty({
+    description: 'JWT access token',
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+  })
   access_token!: string;
 }
